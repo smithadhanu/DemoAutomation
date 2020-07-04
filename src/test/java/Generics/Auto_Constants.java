7b8c1929b287f1104cbb3435bbf9717525e924f5
dhananjaya <64034014+Dhananjaya-bn@users.noreply.github.com>
2020-07-04 23:12:25 +0530

    added auto const page

--- src/test/java/Generics/Auto_Constants.java
+++ src/test/java/Generics/Auto_Constants.java
@@ -7,5 +7,6 @@ public interface Auto_Constants {
 	String chrome_key="webdriver.chrome.driver";
 	String chrome_value="./software/chromedriver.exe";
 	String excel_path="./Excel/data.xlsx";
-	
+	String excel_path1="./Excel/data.xlsx";
+
 }
